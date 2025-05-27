9d97f3ef9d0935acce1d0c2319fa5ae4b3dfe9f5
Li <pinhong.li@dxc.com>
2025-05-27 18:02:48 +0800

    牢

---
+++ ReactTs/bigevent/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div >
+      <div><input placeholder='Username'/></div>
+      <div><input placeholder='Password'/></div>
+      <div><button>Login</button> </div>
+    </div>
+  );
+}
+
+export default App;
