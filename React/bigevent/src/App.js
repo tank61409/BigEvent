9c0c6fb5cc44a12e2634c03633fd6015e9ad5163
Li <pinhong.li@dxc.com>
2025-05-27 16:31:45 +0800

    新增前端react & Vue3

---
+++ React/bigevent/src/App.js
@@ -0,0 +1,14 @@
+import { useRef } from "react";
+import './app.scss'
+
+function App() {
+  return (
+    <div className="App">
+      <div><input placeholder="Username" /></div>
+      <div><input placeholder="Password" /></div>
+      <div><button>Login</button></div>
+    </div>
+  );
+}
+
+export default App;
